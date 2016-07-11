7028ae3af0863c0f374fda64b38a19d83267d600
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-07-11 19:08:05 +0200

    Pass session data to client

--- routes/game.js
+++ routes/game.js
@@ -4,7 +4,11 @@ const router = express.Router()
 router.get("/", (req, res) => {
     if (req.session.player) {
         res.render("game", {
-            player: req.session.player
+            player: JSON.stringify({
+                id: req.session.player.id,
+                name: req.session.player.name,
+                level: req.session.player.level
+            })
         })
     } else {
         res.redirect("/")

f89f4697e5ebe98f80065adda1ea105fdf93fb52
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-07-21 23:31:58 +0200

    Add movement and capture for each piece type

--- public/js/game.js
+++ public/js/game.js
@@ -15,34 +15,222 @@ socket.on("game", (data) => {
     let pieces = [
         {
             "name": "Pawn",
-            "value": 1
+            "value": 1,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 1}, {move: 1, capture: 0}, {move: 0, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 1}, {move: 1, capture: 0}, {move: 0, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Slinger",
-            "value": 2
+            "value": 2,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 1}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 2}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Lancer",
-            "value": 2
+            "value": 2,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 2}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 2}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Archer",
-            "value": 3
+            "value": 3,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 1}, {move: 0, capture: 1}, {move: 0, capture: 1}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 1}],
+                    [{move: 0, capture: 1}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 1}, {move: 0, capture: 2}, {move: 0, capture: 2}, {move: 0, capture: 2}, {move: 0, capture: 1}],
+                    [{move: 0, capture: 2}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 2}],
+                    [{move: 0, capture: 2}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 2}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Lord",
-            "value": 3
+            "value": 3,
+            "action": {
+                "plain": [
+                    [{move: 1, capture: 1}, {move: 1, capture: 1}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}]
+                ],
+                "hill": [
+                    [{move: 1, capture: 1}, {move: 1, capture: 1}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Knight",
-            "value": 3
+            "value": 3,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 1}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 1}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Bishop",
-            "value": 3
+            "value": 3,
+            "action": {
+                "plain": [
+                    [{move: 2, capture: 2}, {move: 0, capture: 0}, {move: 2, capture: 2}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 2, capture: 2}, {move: 0, capture: 0}, {move: 2, capture: 2}]
+                ],
+                "hill": [
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Tower",
-            "value": 4
+            "value": 4,
+            "action": {
+                "plain": [
+                    [{move: 0, capture: 0}, {move: 10, capture: 10}, {move: 0, capture: 0}],
+                    [{move: 10, capture: 10}, {move: 0, capture: 0}, {move: 10, capture: 10}],
+                    [{move: 0, capture: 0}, {move: 10, capture: 10}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "Royal Guard",
-            "value": 4
+            "value": 4,
+            "action": {
+                "plain": [
+                    [{move: 2, capture: 2}, {move: 2, capture: 2}, {move: 2, capture: 2}],
+                    [{move: 2, capture: 2}, {move: 0, capture: 0}, {move: 2, capture: 2}],
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}]
+                ],
+                "hill": [
+                    [{move: 1, capture: 1}, {move: 1, capture: 1}, {move: 1, capture: 1}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 0, capture: 0}, {move: 1, capture: 1}, {move: 0, capture: 0}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         },{
             "name": "General",
-            "value": 5
+            "value": 5,
+            "action": {
+                "plain": [
+                    [{move: 10, capture: 10}, {move: 10, capture: 10}, {move: 10, capture: 10}],
+                    [{move: 10, capture: 10}, {move: 0, capture: 0}, {move: 10, capture: 10}],
+                    [{move: 10, capture: 10}, {move: 10, capture: 10}, {move: 10, capture: 10}]
+                ],
+                "hill": [
+                    [{move: 1, capture: 1}, {move: 1, capture: 1}, {move: 1, capture: 1}],
+                    [{move: 1, capture: 1}, {move: 0, capture: 0}, {move: 1, capture: 1}],
+                    [{move: 1, capture: 1}, {move: 1, capture: 1}, {move: 1, capture: 1}]
+                ],
+                "river": [
+                    [{move: 0, capture: 0}, {move: 1, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}],
+                    [{move: 0, capture: 0}, {move: 0, capture: 0}, {move: 0, capture: 0}]
+                ]
+            }
         }
     ]
 

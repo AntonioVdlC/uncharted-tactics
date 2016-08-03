24dc5e6dc7a34768ff4489ba8483c6737a21f125
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-08-03 17:42:22 +0200

    Correctly render the captured pieces class

--- public/js/game.src/renderCaptured.js
+++ public/js/game.src/renderCaptured.js
@@ -5,7 +5,7 @@ const renderCaptured = function (captured) {
 
     capturedHTML += `<p><span class="captured-back">${5 - captured.back}</span></p>`
     capturedHTML += captured.pieces.reduce((html, piece) => {
-        return html + `<p><span data-type="${piece.type}" class="piece player-${piece.player}" ${dashify(piece.type)}>${piece.type}</span></p>`
+        return html + `<p><span data-type="${piece.type}" class="piece player-${piece.player} ${dashify(piece.type)}">${piece.type}</span></p>`
     }, ``)
 
     return capturedHTML
